bb1ef1895cc3befd7cfef2218ef4ae671cb0fc61
tcrsavage <tcrsavage@gmail.com>
2014-06-03 11:55:05 +1000

    Tweaked admin profile field styling

--- templates/profile-fields.php
+++ templates/profile-fields.php
@@ -30,22 +30,22 @@
 			<th><label for="hma-2fa-secret">Secret codes</label></th>
 			<td id="hma-2fa-secret-settings-fields">
 
-				<div id="hma-2fa-new-secret-fields" style="display: none;">
+				<div id="hma-2fa-new-secret-fields" style=" margin-bottom: 10px; display: none;">
 
-					<h3>1. Download the Google Authenticator app to your device and scan the QR code below</h3>
+					<h4 style="margin-top: 10px;">1. Download the <a href="https://support.google.com/accounts/answer/1066447?hl=en">Google Authenticator</a> app to your device and scan the QR code below</h4>
 
 					<input type="text" name="hma_2fa_secret" id="hma-2fa-secret" placeholder="<?php echo ( $user_2fa->get_secret() ) ? 'It\'s a secret!' : ''; ?>"><br />
 
 					<div id="hma-2fa-qr-code" style="margin: 10px 1px 1px 1px;"></div>
 
-					<h3>2. Store these single use keys somewhere safe, we recommend printing them off</h3>
+					<h4>2. Store these single use keys somewhere safe, we recommend printing them off</h4>
 					<span class="description">You can use these keys for one time access to your account if you lose/break your device</span>
 
 
 					<div id="hma-2fa-single-use-secrets" style="margin-top: 10px; display: none;">
 					</div>
 
-					<h3>3. Type in the code displayed on your Google Authenticator app to confirm that you have completed step 1 correctly</h3>
+					<h4>3. Type in the code displayed on your Google Authenticator app to confirm that you have completed step 1 correctly</h4>
 					<span class="description">You will have 2 minutes after entering this key to submit the update to your profile. Make sure the clock on your device is correct</span>
 
 					<div style="margin-top: 10px;">
@@ -54,8 +54,8 @@
 
 				</div>
 
-				<div style="margin-top: 10px;">
-					<input type="button" id="hma-2fa-generate-secret" value="Generate<?php echo ( $user_2fa->get_secret() ) ? ' new' : ''; ?>"  /> <br />
+				<div>
+					<input type="button" id="hma-2fa-generate-secret" class="button button-secondary" value="Generate<?php echo ( $user_2fa->get_secret() ) ? ' new' : ''; ?>"  /> <br />
 				</div>
 
 			</td>
